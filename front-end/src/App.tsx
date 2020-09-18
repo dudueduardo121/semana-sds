1d5adf222b1d7fa70c78ef3c446a1dcc2317d7d2
dudueduardo121 <dudueduardo121@gmail.com>
2020-09-18 10:29:40 -0300

    create home page

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import Home from './pages/Home/index';
 
 function App() {
   return (
-    <Header/>
+    <div>
+      <Header/>
+      <Home/>
+    </div>
+    
   );
 }
 
